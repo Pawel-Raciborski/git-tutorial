81b4e99216edce58811f21ed7b9cb3e3172e5105
Pawel Raciborski <wojak2845@gmail.com>
2023-08-04 14:55:28 +0200

    commit-4

--- Person.java
+++ Person.java
@@ -2,5 +2,7 @@ public class Person {
     private String name;
     public static void main(String[] args) {
         System.out.println("some text");
+        System.out.println("some text");
+        System.out.println("some text");
     }
 }
