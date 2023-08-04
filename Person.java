40d5d7449d76a26c5462478ad6533d907f1af252
Pawel Raciborski <wojak2845@gmail.com>
2023-08-04 16:54:54 +0200

    commit-6

--- Person.java
+++ Person.java
@@ -2,6 +2,10 @@ public class Person {
     private String name;
     public static void main(String[] args) {
         System.out.println("some text");
+        System.out.println("some text2");
+        System.out.println("some tex2t");
+        System.out.println("some tex2t");
+        System.out.println("some tex2t");
         System.out.println("some text");
         System.out.println("some text");
         System.out.println("some text");
